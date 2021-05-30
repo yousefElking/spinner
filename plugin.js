5fced26bb3a6d50c6570667720ee826987f33ab2
yousefElking <82253879+yousefElking@users.noreply.github.com>
2021-05-30 19:28:06 +0200

    Add files via upload

---
+++ plugin.js
@@ -0,0 +1,17 @@
+function spinnerRun(){
+
+var img =document.getElementById("image");
+
+img.style.animation ="spin 0.1s linear infinite";
+
+
+}
+
+function spinnerStop(){
+
+var img =document.getElementById("image");
+
+img.style.animation ="";
+
+
+}
